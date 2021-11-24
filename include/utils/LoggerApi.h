fc0d597809b1f8a5e89521fef338270e09112d9b
Ruben Sanchez Castellano <tisi1988@gmail.com>
2021-11-24 20:24:38 +0100

    Fixed internal logger API

--- include/utils/LoggerApi.h
+++ include/utils/LoggerApi.h
@@ -1,16 +1,16 @@
 #pragma once
 
+#include "../public/utils/Logger.h"
 #include "LoggerImpl.h"
-#include "public/Logger.h"
 
 #define LOG_INFO(x)                                                            \
-  std::dynamic_casr<LoggerImpl *>(Logger::getLogger())                         \
+  std::dynamic_cast<LoggerImpl *>(Logger::getLogger())                         \
       ->logMessage(LogLevel::INFO, x);
 
 #define LOG_WARN(x)                                                            \
-  std::dynamic_casr<LoggerImpl *>(Logger::getLogger())                         \
+  std::dynamic_cast<LoggerImpl *>(Logger::getLogger())                         \
       ->logMessage(LogLevel::WARN, x);
 
 #define LOG_ERROR(x)                                                           \
-  std::dynamic_casr<LoggerImpl *>(Logger::getLogger())                         \
+  std::dynamic_cast<LoggerImpl *>(Logger::getLogger())                         \
       ->logMessage(LogLevel::ERROR, x);
