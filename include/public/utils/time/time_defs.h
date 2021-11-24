d68ba1d4916c9cdcb8c3248e20fe655c1813c68a
Ruben Sanchez Castellano <tisi1988@gmail.com>
2021-11-24 21:50:30 +0100

    Create time definitions
    Includes the definition of seconds using std::chrono but as a floating point with double precision. Also defines a timebase as a Rational.

---
+++ include/public/utils/time/time_defs.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "public/utils/Rational.h"
+
+#include <chrono>
+
+namespace libffmpegxx {
+namespace utils {
+namespace time {
+/**
+ * Define seconds using std::chrono with double precision.
+ */
+using Seconds = std::chrono::duration<double>;
+
+/**
+ * Define a timebase as a rational.
+ */
+using Timebase = utils::Rational;
+}; // namespace time
+}; // namespace utils
+}; // namespace libffmpegxx
