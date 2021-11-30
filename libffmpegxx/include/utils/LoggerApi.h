b096fc496a8a92d788203dfd7a85d68df30cbd3c
Ruben Sanchez Castellano <tisi1988@gmail.com>
2021-11-30 23:05:56 +0100

    Fixed internal logging API

--- libffmpegxx/include/utils/LoggerApi.h
+++ libffmpegxx/include/utils/LoggerApi.h
@@ -4,13 +4,13 @@
 #include "LoggerImpl.h"
 
 #define LOG_INFO(x)                                                            \
-  std::dynamic_cast<LoggerImpl *>(Logger::getLogger())                         \
-      ->logMessage(LogLevel::INFO, x);
+  dynamic_cast<utils::LoggerImpl *>(Logger::getLogger())                       \
+      ->logMessage(utils::LogLevel::INFO, x);
 
 #define LOG_WARN(x)                                                            \
-  std::dynamic_cast<LoggerImpl *>(Logger::getLogger())                         \
-      ->logMessage(LogLevel::WARN, x);
+  dynamic_cast<utils::LoggerImpl *>(utils::Logger::getLogger())                \
+      ->logMessage(utils::LogLevel::WARN, x);
 
 #define LOG_ERROR(x)                                                           \
-  std::dynamic_cast<LoggerImpl *>(Logger::getLogger())                         \
-      ->logMessage(LogLevel::ERROR, x);
+  dynamic_cast<utils::LoggerImpl *>(Logger::getLogger())                       \
+      ->logMessage(utils::LogLevel::ERROR, x);
